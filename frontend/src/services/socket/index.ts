5864c8dac18a0d280c644b45041f3008cc7bb033
Loren Grixti <loren@beyondplay.io>
2023-11-03 08:36:52 +0100

    initial commit

--- frontend/src/services/socket/index.ts
+++ frontend/src/services/socket/index.ts
@@ -1,18 +1,10 @@
-import { store } from '@/store';
-import pricesSlice from '@/store/pricesSlice';
-
 const connect = () => {
   console.log('CONNECTION STARTING...');
   const ws = new WebSocket('ws://localhost:7777');
   ws.onmessage = function (e) {
     // console.log('Message:', e.data);
     const data = JSON.parse(e.data);
-    store.dispatch(
-      pricesSlice.actions.updatePrice({
-        id: data.id as string,
-        price: data.price
-      })
-    );
+
   };
 
   ws.onclose = function (e) {
